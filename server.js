4e0f68f7f6b09d276d2dae0ab2fe9c6fba4ae378
Kieran Lockyer <kieran.lockyer.87@gmail.com>
2020-01-28 15:21:47 +1000

    working version of the server to facilitate online games

--- server.js
+++ server.js
@@ -7,40 +7,43 @@ const games = []
 wss.on('connection', (connection, req) => {
     connection.on('message', (message) => {
         const data = JSON.parse(message)
-        console.log('message', data)
         if (data.action === 'searching') {
-            console.log('searching')
             const player = data.token
             let openGameFound = false
             for (let game of games) {
                 if (game.white && !game.black) {
                     if (game.white !== player) {
-                        console.log('found an opponent')
                         game.black = player
                         game.connections.push(connection)
                         openGameFound = true
-                        connection.send(JSON.stringify({token: game.black, position: 'black', state: 'begin'}))
+                        connection.send(JSON.stringify({token: game.black, position: 'Black', state: 'begin'}))
                         game.connections[0].send(JSON.stringify({state: 'begin'}))
                         break
                     }
                 }
             }
             if (!openGameFound) {
-                console.log('creating new game')
                 const newGame = { 
                     id: uuidv4(),
                     white: player,
                     connections: [connection]
                 }
                 games.push(newGame)
-                connection.send(JSON.stringify({token: newGame.white, position: 'white'}))
+                connection.send(JSON.stringify({token: newGame.white, position: 'White'}))
             }
-        } else {
-            console.log('move', data.token)
+        } else if (data.action === 'move') {
             for (let game of games) {
                 if (game.white === data.token || game.black === data.token) {
                     for (connection of game.connections) {
                         connection.send(JSON.stringify({move: data.move, token: data.token}))
+                    }
+                }
+            }
+        } else if (data.action === 'promote_pawn') {
+            for (let game of games) {
+                if (game.white === data.token || game.black === data.token) {
+                    for (connection of game.connections) {
+                        connection.send(JSON.stringify({promote_pawn: data.move, token: data.token}))
                     }
                 }
             }
